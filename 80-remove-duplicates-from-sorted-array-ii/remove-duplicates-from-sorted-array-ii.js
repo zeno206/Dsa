9f908b2b51df6c01454b19b097c7216d8431bebf
Shubham Dhiman <shubham.dhiman.mehra@gmail.com>
2026-04-04 13:07:17 +0530

    Time: 62 ms (29.85%) | Memory: 59.4 MB (19.46%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 2 
+    for(let j= 2; j<nums.length;j++){
+        if(nums[j] !== nums[i - 2]){
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i
+};
